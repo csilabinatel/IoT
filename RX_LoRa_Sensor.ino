1430147dabd146911fa7cdd209e011ec9a6686cb
csilabinatel <csi-adm@inatel.br>
2026-04-17 08:51:24 -0300

    Update RX_LoRa_Sensor.ino

--- RX_LoRa_Sensor.ino
+++ RX_LoRa_Sensor.ino
@@ -1,64 +1,66 @@
-#include <SPI.h>
-#include <LoRa.h>
+#include <SPI.h> // Biblioteca para comunicação SPI (usada pelo LoRa)
+#include <LoRa.h>  // Biblioteca para controle do módulo LoRa
 
 // Pinos
-#define NSS 10
-#define RST 9
-#define DIO0 2
+#define NSS 10 // Pino Chip Select (SS)
+#define RST 9  // Pino de reset do módulo LoRa
+#define DIO0 2 // Pino de interrupção (indica pacote recebido)
 
 void setup() {
-  Serial.begin(115200); 
-  while (!Serial);
+  Serial.begin(115200);  // Inicializa a comunicação serial  115200
+  while (!Serial); // Aguarda a serial iniciar (útil em algumas placas)
 
-  LoRa.setPins(NSS, RST, DIO0);
 
+  LoRa.setPins(NSS, RST, DIO0); // Define os pinos do LoRa
+
+  // Inicializa o LoRa na frequência de 915 MHz
   if (!LoRa.begin(915E6)) {
-    Serial.println("Erro LoRa");
-    while (true);
+    Serial.println("Erro LoRa");// Mensagem de erro se não inicializar
+    while (true);// Trava o programa se der erro
   }
 
   
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
-  LoRa.enableCrc();
+  LoRa.setSpreadingFactor(7); // Define o Spreading Factor (velocidade vs alcance)
+  LoRa.setSignalBandwidth(125E3); // Define largura de banda (125 kHz)
+  LoRa.setCodingRate4(5);// Define taxa de codificação (correção de erro)
+  LoRa.enableCrc(); // Ativa verificação de erro (CRC)
 
-  Serial.println("RX OK");
+  Serial.println("RX OK");// Indica que o receptor está pronto
 }
 
 void loop() {
 
-  int packetSize = LoRa.parsePacket();
+  int packetSize = LoRa.parsePacket();// Verifica se chegou algum pacote
 
-  if (packetSize) {
+  if (packetSize) {// Se um pacote foi recebido
 
-    String msg = "";
+    String msg = ""; // String para armazenar a mensagem recebida
 
-    while (LoRa.available()) {
-      char c = (char)LoRa.read();
+    while (LoRa.available()) { // Enquanto houver dados disponíveis
+      char c = (char)LoRa.read();// Lê um caractere do buffer
 
-      // 🔹 filtro
+      //filtro
       if (c >= 32 && c <= 126) {
         msg += c;
       }
     }
 
     if (msg.length() < 5) return;
-    if (!msg.startsWith("Grupo1:")) return;
+    if (!msg.startsWith("Grupo1:")) return;// Ignora mensagens que não começam com "Grupo1:"
 
     String dado = msg.substring(7);
 
-    if (dado.startsWith("Solo Umido")) {
+    if (dado.startsWith("Solo Umido")) { // Verifica se o solo está úmido
 
       Serial.print("UMIDO -> ");
       Serial.println(dado);
 
     } else {
 
-      int valor = dado.toInt();
+      int valor = dado.toInt(); // Converte o dado para número (caso seja valor de sensor)
 
-      Serial.print("SECO -> ");
-      Serial.println(valor);
+      Serial.print("SECO -> "); // Indica solo seco
+      Serial.println(valor); // Mostra o valor recebido
     }
   }
 }
