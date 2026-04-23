79b41a5e7cae7eb1b29dcfc809bd16fa7afa73d5
csilabinatel <csi-adm@inatel.br>
2026-04-23 11:55:26 -0300

    Update RX_LoRa_Sensor.ino

--- RX_LoRa_Sensor.ino
+++ RX_LoRa_Sensor.ino
@@ -1,13 +1,14 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-// Pinos para Arduino Uno
-#define NSS 10
-#define RST 9
-#define DIO0 2
+// Definição dos pinos
+#define NSS 10          // Pino Chip Select (CS) do LoRa //pino que ouve o Ligar do LoRa"conversa"
+#define RST 9           // Pino de RESET do módulo LoRa
+#define DIO0 2          // Pino de interrupção (DIO0,com a biblioteca) do LoRa (indica eventos como envio/recebimento) sem precisar dos calculos do tempo 
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);//Inicialização do LoRa                                                   
   while (!Serial);
 
   LoRa.setPins(NSS, RST, DIO0);
@@ -18,13 +19,12 @@ void setup() {
     while (true);
   }
 
-  // --- OTIMIZAÇÃO DE VELOCIDADE ---
-  // SF7 é o mais rápido. Se quiser MAIS rápido, use 6 (mas exige precisão de cristal)
-  LoRa.setSpreadingFactor(7); 
-  // Largura de banda de 125E3 é padrão. 250E3 seria mais rápido, mas 125 é mais estável.
-  LoRa.setSignalBandwidth(125E3); 
-  LoRa.setCodingRate4(5);
-  LoRa.enableCrc();
+  // --- OTIMIZAÇÃO DE VELOCIDADE  IGUAL AO TX ---
+  LoRa.setSpreadingFactor(7);//(7 Velocidade alta com alcance menor)Quantos bits são usados para representar cada símbolo,afeta diretamente alcance e velocidade = 2^7=128;
+  // Largura de banda de 125E3 é padrão.
+  LoRa.setSignalBandwidth(125E3); //Tamanho da faixa de Frêquencia
+  LoRa.setCodingRate4(5);//Envia Bits extras para uma correção
+  LoRa.enableCrc();//Verificação de erro(Verifica o que foi transmitido com o que está nele)
 
   Serial.println("RX veloz iniciado. Aguardando Grupo 1...");
 }
@@ -38,11 +38,11 @@ void loop() {
 
     // Lê o pacote caractere por caractere
     while (LoRa.available()) {
-      recebido += (char)LoRa.read();
+      recebido += (char)LoRa.read();//LEITURA DO PACOTE BIT POR BIT 
     }
 
     // Filtra apenas o Grupo 1
-    if (recebido.startsWith("Grupo1:")) {
+    if (recebido.startsWith("Grupo1:"))//Ele verifica se o começo do pacote começa com o nosso grupo{
       // Isola o valor após os 7 caracteres de "Grupo1:"
       String valor = recebido.substring(7);
       
