110ca0f544ab96b26e2eb158b58e981edd4a0ced
csilabinatel <csi-adm@inatel.br>
2026-04-23 11:42:18 -0300

    Update LoRa_TX_Sensor.ino

--- LoRa_TX_Sensor.ino
+++ LoRa_TX_Sensor.ino
@@ -2,9 +2,9 @@
 #include <LoRa.h>       // Biblioteca para controle do módulo LoRa (SX1276)
 
 // Definição dos pinos
-#define NSS 10          // Pino Chip Select (CS) do LoRa
+#define NSS 10          // Pino Chip Select (CS) do LoRa //pino que ouve o Ligar do LoRa"conversa"
 #define RST 9           // Pino de RESET do módulo LoRa
-#define DIO0 2          // Pino de interrupção (DIO0) do LoRa (indica eventos como envio/recebimento)
+#define DIO0 2          // Pino de interrupção (DIO0,com a biblioteca) do LoRa (indica eventos como envio/recebimento) sem precisar dos calculos do tempo 
 
 #define SENSOR A5       // Pino analógico A5 onde está conectado o sensor de umidade
 float umidade2 = 0;     // Variável para armazenar valor de umidade (uso auxiliar)
@@ -21,10 +21,10 @@ void setup() {
     Serial.println("Erro ao iniciar LoRa"); // Mostra erro se não conseguir iniciar
     while (true);                // Trava o programa caso haja falha
   }
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
-  LoRa.enableCrc();
+  LoRa.setSpreadingFactor(7);(7 Velocidade alta com alcance menor)Quantos bits são usados para representar cada símbolo,afeta diretamente alcance e velocidade.
+  LoRa.setSignalBandwidth(125E3);//Velocidade do Pacote na Transmissão
+  LoRa.setCodingRate4(5);//Envia Bits extras para uma correçãon
+  LoRa.enableCrc();//Verificação de erro(Verifica o que foi transmitido com o que está nele)
 
   Serial.println("LoRa transmissor iniciado"); // Confirma inicialização com sucesso
 }
@@ -33,10 +33,11 @@ void loop() {
   float umidade = analogRead(SENSOR);
 
   // ENVIO IGUAL PARA OS DOIS CASOS
-  LoRa.beginPacket();
-  LoRa.print("Grupo1:");
-  LoRa.println(umidade); 
-  LoRa.endPacket();
+  LoRa.beginPacket();//Começa o pacote 
+  LoRa.print("Grupo1:");//Indentifica o pacote
+  LoRa.println(umidade); //envia o valor do sensor
+  //LoRa.print(";");         // separador
+  LoRa.endPacket();// fim do pacote"Grupo1:523"
 
   // SERIAL IGUAL PARA OS DOIS CASOS
   Serial.print("Enviado: ");
